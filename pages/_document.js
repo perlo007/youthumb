5182d6abd6c8838f74f0c9f8062c88aa1a2c8e66
perlo007 <perlocasa@gmail.com>
2024-07-16 15:06:10 +0100

    add adsense

--- pages/_document.js
+++ pages/_document.js
@@ -22,9 +22,8 @@ class MyDocument extends Document {
             sizes="16x16"
             href="/favicon/favicon-16x16.png"
           />
-          
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <script data-ad-client="ca-pub-XXXXXXXXXXXXXXXX" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+          <link rel="manifest" href="/favicon/site.webmanifest" />
+          <script data-ad-client="ca-pub-XXXXXXXXXXXXXXXX" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         </Head>
         <body>
           <Main />
